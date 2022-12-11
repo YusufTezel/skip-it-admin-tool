773e500a2aae02e37784be6a6a0d31f65dca658f
yusuf-tezel <pza.corner.app@gmail.com>
2022-12-11 23:26:41 +0100

    save work

--- src/store/features/enterprise/enterpriseItem.jsx
+++ src/store/features/enterprise/enterpriseItem.jsx
@@ -1,40 +1,64 @@
 import React from 'react'
 import TreeItem from '@mui/lab/TreeItem';
 import TreeView from '@mui/lab/TreeView';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
 function RecursiveFunction(obj) {
     const items = obj.items;
-
-    for (let index = 0; index < items.length; index++) {
-        const element = items[index];
-
-        if (element.childs && element.childs.length === 0) {
-            return (
-                <TreeItem nodeId={element.id} label={element.name}></TreeItem>
-            );
+    return items.map(element => {
+        if (element.childs.length === 0) {
+            return <TreeItem key={element.id} nodeId={element.id} label={element.name}></TreeItem>
         }
 
         return (
-            <TreeItem nodeId={element.id} label={element.name}>
+            <TreeItem key={element.id} nodeId={element.id} label={element.name}>
                 <RecursiveFunction items={element.childs}></RecursiveFunction>
             </TreeItem>
-        );
-        
-    }
+        )
+    })
 }
 
 export function EnterpriseItem() {
 
     const e = [{
-        name: "Something",
-        id: "1",
+        name: "Tryg - 70112020",
+        id: "yaijdwquw-wdomdd",
         childs: [{
-            name: "anotherthing",
-            id: "2",
+            name: "0 - Englesk",
+            id: "qowfmpqwkd-wpkqdmoiqwmd",
             childs: [{
-                id: "3",
-                name: "thirdthing",
+                id: "koqwfm-omdqopmd",
+                name: "1 - Ny kunde",
                 childs: []
+            }, {
+                id: "kwdm-kwmd",
+                name: "2 - Sunhed",
+                childs: [{
+                    id: "owpqwww-lsllsko",
+                    name: "1 - Gør brug",
+                    childs: []
+                }]
+            }]
+        }],
+    }, {
+        name: "Danske bank - 72304050",
+        id: "6",
+        childs: [{
+            name: "1 - Pension",
+            id: "7",
+            childs: [{
+                id: "8",
+                name: "1 - Førtidspension",
+                childs: []
+            }, {
+                id: "9",
+                name: "2 - Opsparing",
+                childs: [{
+                    id: "10",
+                    name: "1 - Formue",
+                    childs: []
+                }]
             }]
         }]
     }];
@@ -42,6 +66,8 @@ export function EnterpriseItem() {
     return (
         <TreeView
             aria-label="file system navigator"
+            defaultCollapseIcon={<ExpandMoreIcon />}
+            defaultExpandIcon={<ChevronRightIcon />}
             sx={{ height: "60vh", flexGrow: 1, overflowY: 'auto', marginTop: '7px' }}>
             <RecursiveFunction items={e}></RecursiveFunction>
         </TreeView>
